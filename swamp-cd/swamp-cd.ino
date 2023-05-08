aced98d5abc7e5c1745d0979d70bbc9932eca087
cdacanay <cdacanay@nevada.unr.edu>
2023-05-07 23:54:40 -0700

    State LEDs, vent control buttons, on/off interrupt button

--- swamp-cd/swamp-cd.ino
+++ swamp-cd/swamp-cd.ino
@@ -11,7 +11,7 @@
 #define MONTH 5   //set month num
 #define DAY 9   //set day num
 #define YEAR 23   //set year 20XX
-#define HOUR 5    //set hour num 0-24
+#define HOUR 12    //set hour num 0-24
 #define MINUTE 0    //set minute num
 
 volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
@@ -23,6 +23,16 @@ volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 volatile unsigned char* port_b = (unsigned char*) 0x25;
 volatile unsigned char* ddr_b  = (unsigned char*) 0x24;
 volatile unsigned char* pin_b  = (unsigned char*) 0x23;
+volatile unsigned char* port_l = (unsigned char*) 0x10B;
+volatile unsigned char* ddr_l  = (unsigned char*) 0x10A;
+volatile unsigned char* pin_l  = (unsigned char*) 0x109;
+volatile unsigned char* port_d = (unsigned char*) 0x2B;
+volatile unsigned char* ddr_d  = (unsigned char*) 0x2A;
+volatile unsigned char* pin_d  = (unsigned char*) 0x29;
+
+volatile unsigned char* my_EICRA = (unsigned char*) 0x69;
+volatile unsigned char* my_EIMSK  = (unsigned char*) 0x3D;
+
 
 volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
@@ -30,7 +40,8 @@ volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 
 enum State {DISABLED = 0, ERROR = 1, IDLE = 2, RUNNING = 3};
-enum State currentState = IDLE;
+volatile enum State currentState = IDLE;
+volatile bool togglePower = 0;
 
 
 dht DHT;
@@ -39,19 +50,47 @@ Stepper stepper = Stepper(64, 12, 10, 11, 9);
 uRTCLib rtc(0x68);
 
 void setup() {
+  //port setup
+  //*ddr_b |= 0x01; //pb0 output (fan motor)
+  //*ddr_b &= 0xFB; //pb2 input (stepper motor button)
+  //*ddr_b &= 0xF7; //pb3 input (stepper motor button)
+  //*ddr_b |= 0x10; //pb4 output (stepper motor)
+
+  *ddr_b &= 0xF3; //pb2+3 input (stepper motor buttons)
+  *ddr_b |= 0x11; //pb0+4 output (fan motor + stepper motor)
+  *ddr_l |= 0x0F; //pl0 to pl3 output (state leds)  (pl0 = yellow, pl1 = red, pl2 = green, pl3 = blue)
+  *ddr_d &= 0x08; //pd3 input (on/off)
+
+  *my_EICRA |= 0xC0;  //set interrupt request on rising edge
+  *my_EIMSK |= 0x08;  //enable pin interrupt on pd3 (int3)
+
   U0init(9600);
   adc_init();
   lcd.begin(16, 2);
-  *ddr_b |= 0x10; //pb4 output
-  *ddr_b |= 0x01; //pb0 output
   URTCLIB_WIRE.begin();
   rtc.set(0, MINUTE, HOUR, 2, DAY, MONTH, YEAR);
+
 }
 
 
+
 void loop() {
+  if(togglePower) {
+    if(currentState > DISABLED) {
+      setState(DISABLED);
+    }
+    else {
+      setState(IDLE);
+    }
+    togglePower = 0;
+  }
 
   if(currentState == RUNNING) {
+    if(!(*port_l & 0x08)) {
+      *port_l |= 0x08;  //turn on blue led
+      *port_l &= 0xF8;  //turn off other leds
+    }
+
     //fan
     if(!(*port_b & 0x01)) {
       *port_b |= 0x01;  //pb0 high
@@ -77,8 +116,8 @@ void loop() {
     *port_b &= 0xEF;  //pb4 plow
     //float voltage_float = adc_voltage * 5.0 / 1024.0; //convert 0-1023 to 0-5.0V
     int water_level = adc_voltage / 2;
-    U0putint(water_level);
-    U0putchar('\n');
+    //U0putint(water_level);
+    //U0putchar('\n');
 
     //Serial output
     //U0putVoltage(voltage_float);
@@ -102,38 +141,94 @@ void loop() {
   }
 
   if(currentState == IDLE) {
+    if(!(*port_l & 0x04)) {
+      *port_l |= 0x04;  //turn on green led
+      *port_l &= 0xF4;  //turn off other leds
+    }
+    
     if(DHT.temperature > TARGET_TEMP + 1) {
       currentState = RUNNING;
     }
   }
   
   if(currentState == ERROR) {
+    if(!(*port_l & 0x02)) {
+      *port_l |= 0x02;  //turn on red led
+      *port_l &= 0xF2;  //turn off other leds
+    }
+
     lcd.clear();
     lcd.print("ERROR: Water Level Low");
     
     U0putStringLn("Water Level Low");
   }
 
+  if(currentState == DISABLED) {
+    if(!(*port_l & 0x01)) {
+      *port_l |= 0x01;  //turn on red led
+      *port_l &= 0xF1;  //turn off other leds
+    }
+  }
+
+  //vent control (stepper motor)
+  if(currentState > DISABLED) {
+    if(*port_b & 0x0C) {}   //if both bottons pressed, do nothing
+    else if(*pin_b & 0x04) {
+      stepper.setSpeed(300);
+      stepper.step(256);
+    }
+    else if(*pin_b & 0x08) {
+      stepper.setSpeed(300);
+      stepper.step(-256);
+    }
+  }
+
 
   //U0putint(currentState);
 
 
-  //stepper.setSpeed(300);
-  //stepper.step(2048);
-  getClock();
-  delay(2000);
+  
+  //getClock();
+  delay(1000);
 
+}
+
+
+ISR(INT3_vect) {  //interrupt for on/off
+  togglePower = 1;  //power toggle at beginning of loop, not done directly in case interrupt happens right before another state change
+}
+
+void setState(enum State state) { //set state and print timestamp
+  currentState = state;
+  String stateString;
+  switch(state) {
+    case DISABLED:  //turn state into string for serial output
+      stateString = "Disabled";
+      break;
+    case ERROR:
+      stateString = "Error";
+      break;
+    case IDLE:
+      stateString = "Idle";
+      break;
+    case RUNNING:
+      stateString = "Running";
+  }
+  U0putString("Status: ");
+  U0putString(stateString);
+  U0putString(" @ ");
+  getClock();
 }
 
 void getClock() {
   rtc.refresh();
   U0putint(rtc.hour());
   U0putchar(':');
-  if(rtc.minute() < 10) {
+  if(rtc.minute() < 10) { //display :00 instead of :0
     U0putchar('0');
   }
   U0putint(rtc.minute());
-  U0putString(" ");
+  U0putchar(' ');
   U0putint(rtc.month());
   U0putchar('/');
   U0putint(rtc.day());
